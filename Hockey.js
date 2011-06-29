ed8d77024263acb2caa1830d5ad7d6186aed4e6b
Daniel X. Moore <yahivin@gmail.com>
2011-06-29 08:30:38 +0000

    Title Screen

--- Hockey.js
+++ Hockey.js
@@ -19322,20 +19322,51 @@ Zamboni = function(I) {
 };;
 var TitleScreen;
 TitleScreen = function(I) {
-  var directory, titleScreen, _ref;
+  var directory, loadingText, titleScreen, titleScreenImage, titleScreenText, _ref;
+  $.reverseMerge(I, {
+    backgroundColor: "#00010D",
+    callback: $.noop
+  });
   directory = (typeof App !== "undefined" && App !== null ? (_ref = App.directories) != null ? _ref.images : void 0 : void 0) || "images";
-  titleScreen = $("<img />", {
+  titleScreen = $("<div />", {
     css: {
+      backgroundColor: I.backgroundColor,
+      fontFamily: "monospace",
+      fontSize: "20px",
+      fontWeight: "bold",
       left: 0,
       margin: "auto",
       position: "absolute",
+      textAlign: "center",
       top: 0,
       zIndex: 1000
-    },
+    }
+  }).appendTo("body");
+  titleScreenImage = $("<img />", {
     height: App.height,
     src: "" + BASE_URL + "/" + directory + "/title.png",
     width: App.width
-  }).appendTo("body");
+  }).appendTo(titleScreen);
+  loadingText = $("<div />", {
+    text: "Loading...",
+    css: {
+      bottom: "40%",
+      color: "#FFF",
+      position: "absolute",
+      width: "100%",
+      zIndex: -1
+    }
+  }).appendTo(titleScreen);
+  titleScreenText = $("<div />", {
+    text: "Press any key to begin",
+    css: {
+      bottom: "12.5%",
+      color: "#00010D",
+      position: "absolute",
+      width: "100%",
+      zIndex: 1
+    }
+  }).appendTo(titleScreen);
   return $(document).one("keydown", function() {
     titleScreen.remove();
     return I.callback();
